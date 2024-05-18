97cdbc9d1e3973824c34d5f351480b42351fa8a9
ubaid Dev <obaidkhanzada2004@gmail.com>
2024-05-18 05:16:50 +0500

    add fationality & fetch data

--- src/Pages/Location.jsx
+++ src/Pages/Location.jsx
@@ -1,79 +1,125 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import App from "./../App";
 import MenuBar from "../Components/Menu";
 import { TextField } from "@mui/material";
 import { Input, Space } from "antd";
 import img from "../Images/sun.png";
 const { Search } = Input;
-// import { faTemperatureHalf } from '@fortawesome/react-fontawesome'
+import { Empty, Transfer } from "antd";
 import "../Components/Style.css";
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function Location() {
+  const [location, setLocation] = useState("");
+  const [data, setData] = useState("");
+  const [weatherCast, setforeCast] = useState([]);
+  const API_KEY = `de7bf3dd53bd737f80a064ec0b825fb3`;
+
+  const onBtnCall = () => {
+    console.log("first");
+    getWeather(location);
+  };
+
+  const getWeather = async (location) => {
+    let url;
+    let foreCastURL;
+    if (location) {
+      url = `https://api.openweathermap.org/data/2.5/weather?&q=${location}&appid=${API_KEY}&units=metric`;
+      foreCastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=metric`;
+    } else {
+      try {
+        const position = await getCurrentPosition();
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+        foreCastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+      setLocation("");
+      console.log("Weather Data:", result); // Check the data here
+      const fetchForecast = await fetch(foreCastURL);
+      const resForecast = await fetchForecast.json();
+      setforeCast(resForecast);
+      console.log("Forecast Data:", resForecast); // Check the forecast data here
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  let iconImg = data?.weather?.[0]?.icon;
+
+  const getCurrentPosition = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <>
-      {/* <div className="flex container  main bg-slate-500 ">
-        <div>
-          <MenuBar />
-        </div>
-
-        <div className="flex ">
-          <Search placeholder="input search text" 
-          style={{
-            width: 600,
-            color:"#202B3B",
-            border:"grey"
-          }}
-          size="large"
-          allowClear
-          enterButton />
-        </div>
-        <div className="flex">
-          <div className="bg-slate-300">
-          <div>
-          <span>Pakistan</span>
-          <span>chance of rain : 0% </span>
-          </div>
-          <div>
-          <span>31&deg; </span>
-          </div>
-          </div>
-          <div  className="bg-red-950">
-          </div>
-        </div>
-      </div> */}
       <div className="main">
-        <span>
-          <Search
-            placeholder="input search text"
-            style={{
-              width: 600,
-              color: "#202B3B",
-              border: "grey",
-              margin: "20px",
+        <span className="flex items-center mt-1 font-bold ml-20  rounded-l-lg">
+          <TextField
+            id="filled-search"
+            label="Enter your Location"
+            type="search"
+            variant="filled"
+            value={location}
+            onChange={(e) => {
+              setLocation(e.target.value);
             }}
-            size="large"
-            allowClear
-            enterButton
+            style={{ background: "#FFFFFF", flexGrow: 0.5, color: "#D1D5DB",}}
           />
+          <span
+            style={{
+              background: "#FFFFFF",
+              width: "50px",
+              height: "56px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              cursor:"pointer"
+            }}
+            onClick={onBtnCall}
+            className="w-45"
+          >
+            <i className="text-gray-600 fa-solid fa-2x fa-magnifying-glass"></i>
+          </span>
         </span>
-        <div className=" flex " style={{ color: "#C4C9D2" }}>
-          <div className="temp ">
-            <div className="flex flex-row lg:m-20 m-5  items-center  justify-between">
-              <div className="flex flex-col ">
+        {data && data.name ?(
+        <div className="flex flex-wrap" style={{ color: "#C4C9D2" }}>
+          <div className="temp md:grow sm:grow">
+            <div className="flex flex-row m-5 lg-m-9 items-center  justify-between">
+              <div className="flex  flex-col ">
                 <div>
-                  <span className="text-[32px] font-black font-sans text-2xl text-white">
-                    Karachi
+                  <span className="text-[39px]  font-black font-sans text-2xl text-white">
+                    {data.name}
                   </span>
                 </div>
-                <div className="mt-9">
-                  <span className="text-white font-black text-[25px]">
-                    31 &deg;
+                  <span className="">Chance of rain  {data.clouds && `${data.clouds.all}%`}</span>
+                <div className="mt-5">
+                  <span className="text-white font-black text-[34px]">
+                    {data.main && `${Math.floor(data.main.temp)}°`}
                   </span>
                 </div>
               </div>
               <div>
-                <img src={img} className="w-20 " alt="" />
+                <img
+                style={{
+                  width:"155px"
+                }}
+                  src={`https://openweathermap.org/img/wn/${iconImg}.png`}
+                  className="img-main"
+                  alt="cloud Image"
+                />
               </div>
             </div>
             <div
@@ -81,190 +127,174 @@ function Location() {
               style={{
                 background: "#202B3B",
                 borderRadius: "10px",
-                margin: "7",
               }}
             >
-              <span className="">TODAYS FORECAST </span>
-              <div className="flex flex-shrink">
-                <div
-                  style={{ background: "#202B3B", borderBlockColor: "#202B3B" }}
-                  className=" border-r-2  m-4"
-                >
-                  <span>6:00 AM</span>
-                  <span>
-                    <img src={img} className="img-icon" alt="" />
-                  </span>
-                  <span className="font-bold">20 &deg;</span>
-                </div>
-                <div
-                  style={{ background: "#202B3B", borderBlockColor: "#202B3B" }}
-                  className=" border-r-2  m-4"
-                >
-                  <span>6:00 AM</span>
-                  <span>
-                    <img src={img} className="img-icon" alt="" />
-                  </span>
-                  <span className="font-bold">20 &deg;</span>
-                </div>
-                <div
-                  style={{ background: "#202B3B", borderBlockColor: "#202B3B" }}
-                  className=" border-r-2  m-4"
-                >
-                  <span>6:00 AM</span>
-                  <span>
-                    <img src={img} className="img-icon" alt="" />
-                  </span>
-                  <span className="font-bold">20 &deg;</span>
-                </div>
-                <div
-                  style={{ background: "#202B3B", borderBlockColor: "#202B3B" }}
-                  className=" border-r-2  m-4"
-                >
-                  <span>6:00 AM</span>
-                  <span>
-                    <img src={img} className="img-icon" alt="" />
-                  </span>
-                  <span className="font-bold">20 &deg;</span>
-                </div>
-                <div
-                  style={{ background: "#202B3B", borderBlockColor: "#202B3B" }}
-                  className=" border-r-2  m-4"
-                >
-                  <span>6:00 AM</span>
-                  <span>
-                    <img src={img} className="img-icon" alt="" />
-                  </span>
-                  <span className="font-bold">20 &deg;</span>
-                </div>
-                <div
-                  style={{ background: "#202B3B", borderBlockColor: "#202B3B" }}
-                  className=" border-r-2  m-4"
-                >
-                  <span>6:00 AM</span>
-                  <span>
-                    <img src={img} className="img-icon" alt="" />
-                  </span>
-                  <span className="font-bold">20 &deg;</span>
-                </div>
+              <span
+                className="font-semibold text-sm md:text-center"
+                style={{ color: "#C4C9D2" }}
+              >
+                TODAYS FORECAST
+              </span>
+              <div className="flex shrink justify-center items-center flex-wrap">
+                {/* Example forecast data display */}
+                {weatherCast.list &&
+                  weatherCast.list.slice(0, 6).map((forecast, index) => (
+                    <div
+                      key={index}
+                      style={{
+                        background: "#202B3B",
+                        borderBlockColor: "#202B3B",
+                      }}
+                      className="border-r border-r-gray-700 p-3 mr-1 text-center pb-3 m-2"
+                    >
+                      <span
+                        style={{
+                          color: "#C4C9D2",
+                          fontSize: "13px",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        {new Date(forecast.dt * 1000).toLocaleTimeString()}
+                      </span>
+                      <span>
+                        <img
+                          src={`https://openweathermap.org/img/wn/${iconImg}.png`}
+                          className="img-icon"
+                          alt=""
+                        />
+                      </span>
+                      <span className="font-extrabold text-lg">
+                        {Math.floor(forecast.main.temp)}&deg;
+                      </span>
+                    </div>
+                  ))}
               </div>
             </div>
-            <div className="m-3 p-3 rounded-lg"
-                  style={{ background: "#202B3B" }}
-            
+            <div
+              className="m-3 p-3 rounded-lg"
+              style={{ background: "#202B3B" }}
             >
-              <div className=""> 
-                <span className="font-bold">AIR CONDITIONS</span>
+              <div>
+                <span
+                  className="font-semibold text-sm"
+                  style={{ color: "#C4C9D2" }}
+                >
+                  AIR CONDITIONS
+                </span>
                 <div className="flex flex-row justify-around">
                   <div className="flex flex-col">
-                    <span><i class="fa-solid fa-temperature-half"></i> <span className="text-md">Real Feel</span></span>
-                    <span className="font-bold text-2xl">20 &deg;</span>
-                    
-                    <span><i class="fa-solid fa-droplet"></i> <span>Chance of rain</span></span>
-                    
-                    <span className="font-bold text-2xl">20%</span>
-
+                    <span>
+                      <i className="fa-solid fa-temperature-half"></i>{" "}
+                      <span className="text-md">Real Feel</span>
+                    </span>
+                    <span className="font-bold text-2xl">
+                      {data.main && `${data.main.feels_like}°`}
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-droplet"></i>{" "}
+                      <span>Chance of rain</span>
+                    </span>
+                    <span className="font-bold text-2xl">
+                      {data.clouds && `${data.clouds.all}%`}
+                    </span>
                   </div>
                   <div>
-                    <div>
-                      <div className="flex flex-col">
-                        <span> <i class="fa-solid fa-wind"></i> <span>wind</span></span>
-                        
-                        <span className="font-bold text-2xl">0.2 km/h</span>
-
-                        <span><i class="fa-solid fa-gear"></i> <span>UV Index</span></span>
-                        
-                        <span className="font-bold text-2xl">3</span>
-                        
-                      </div>
+                    <div className="flex flex-col">
+                      <span>
+                        <i className="fa-solid fa-wind"></i> <span>wind</span>
+                      </span>
+                      <span className="font-bold text-2xl">
+                        {data.wind && `${data.wind.speed} km/h`}
+                      </span>
+                      <span>
+                        <i class="fa-solid fa-water"></i>
+                        <span> Humidity</span>
+                      </span>
+                      <span className="font-bold text-2xl">{data?.main?.humidity}</span>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="fore-cast bg-slate-800">
+          <div className="fore-cast md:grow sm:grow">
             <span
               style={{ color: "#C4C9D2" }}
-              className="p-9 text-[19] font-medium text-base"
+              className="p-9 text-[19] font-medium sm:text-base lg:text-lg lg:mt-10"
             >
-              {" "}
               7 days forecast
             </span>
-            <div className="flex today">
-              <span>Today</span>
-              <span>
-                <img src={img} className="img-icon" alt="" />
-              </span>
-              <span>Sunny</span>
-              <span>35/21</span>
-            </div>
-            <div className="flex today">
-              <span>Today</span>
-              <span>
-                <img src={img} className="img-icon" alt="" />
-              </span>
-              <span>Sunny</span>
-              <span>35/21</span>
-            </div>
-            <div className="flex today">
-              <span>Today</span>
-              <span>
-                <img src={img} className="img-icon" alt="" />
-              </span>
-              <span>Sunny</span>
-              <span>35/22</span>
-            </div>
-            <div className="flex today">
-              <span>Today</span>
-              <span>
-                <img src={img} className="img-icon" alt="" />
-              </span>
-              <span>Sunny</span>
-              <span>35/23</span>
-            </div>
-            <div className="flex today">
-              <span>Today</span>
-              <span>
-                <img src={img} className="img-icon" alt="" />
-              </span>
-              <span>Sunny</span>
-              <span>35/24</span>
-            </div>
-            <div className="flex today">
-              <span>Today</span>
-              <span>
-                <img src={img} className="img-icon" alt="" />
-              </span>
-              <span>Sunny</span>
-              <span>35/25</span>
-            </div>
-            <div className="flex today">
-              <span>Today</span>
-              <span>
-                <img src={img} className="img-icon" alt="" />
-              </span>
-              <span>Sunny</span>
-              <span>35/25</span>
-            </div>
-            <div className="flex today">
-              <span>Today</span>
-              <span>
-                <img src={img} className="img-icon" alt="" />
-              </span>
-              <span>Sunny</span>
-              <span>35/26</span>
-            </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            {weatherCast.list &&
+              weatherCast.list.slice(0, 7).map((forecast, index) => (
+                <div
+                  key={index}
+                  className="flex today border-b border-b-gray-700 pb-2"
+                >
+                  <span className="text-sm">
+                    {new Date(forecast.dt * 1000).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <img
+                      src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
+                      className="img-icon"
+                      alt=""
+                    />
+                  </span>
+                  <span className="text-base font-medium">
+                    {forecast.weather[0].main}
+                  </span>
+                  <span><b>{`${Math.floor(forecast.main.temp_max)}/${Math.floor(forecast.main.temp_min)}`}</b></span>
+                </div>
+              ))}
           </div>
-        </div>
+        </div>):(
+         <div
+         className="flex  flex-col justify-center"
+         style={{
+           background: "#E6F4FF",
+           width: "440px",
+           height: "500px",
+           borderRadius: "20px",
+           marginLeft: "190px",
+         }}
+       >
+         <div className="flex items-center flex-col ">
+           <Empty  image={Empty.PRESENTED_IMAGE_SIMPLE} />
+           <div>
+             <span className="font-extrabold  text-50 ">Not found !</span>
+             <span className="font-bold bold text-40">
+               Please Enter Valid Country or City
+             </span>
+           </div>
+         </div>
+       </div>  
+        )}
       </div>
     </>
   );
 }
 
 export default Location;
+
+{
+  /* <div
+  className="flex  flex-col justify-center"
+  style={{
+    background: "#E6F4FF",
+    width: "440px",
+    height: "500px",
+    borderRadius: "20px",
+    marginLeft: "190px",
+  }}
+>
+  <div className="flex items-center flex-col ">
+    <Empty  image={Empty.PRESENTED_IMAGE_SIMPLE} />
+    <div>
+      <span className="font-extrabold  text-50 ">Not found !</span>
+      <span className="font-bold bold text-40">
+        Please Enter Valid Country or City
+      </span>
+    </div>
+  </div>
+</div> */
+}
